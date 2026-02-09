4cc5369d239f21e788ce7a362a26b1cce5107c89
Vishnu14832 <vishnusrivastava442@gmail.com>
2026-02-10 00:06:17 +0530

    final production settings

--- hrms/settings.py
+++ hrms/settings.py
@@ -12,7 +12,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY SETTINGS
 SECRET_KEY = os.environ.get('SECRET_KEY', 'fallback-secret-key')
 
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', 'False') == 'True'
+
 
 ALLOWED_HOSTS = ['*']
 
